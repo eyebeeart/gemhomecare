b5570e4f91cdf013e1bfaf966d8b20ea8340db21
eyebeeart <115160968+eyebeeart@users.noreply.github.com>
2025-05-16 01:24:44 +0100

    update

--- script.js
+++ script.js
@@ -42,10 +42,83 @@ document.addEventListener('click', (e) => {
         toggle.style.display = "block";
         toggle.style.transform = "scale(1)"
     },300);
-
+    return;
 }
 });
 
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    })
+});
+
+
+
+
+
+
+    // Create an IntersectionObserver to animate flex-box items when they enter the viewport
+    const observer = new IntersectionObserver((entries, observer) => { 
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {  // Check if the container is visible
+               const items = entry.target.querySelectorAll('.service-card');
+                items.forEach((item, index) => {
+                    setInterval(() => {
+                        item.classList.add('show'); // Add animation class
+                    }, index * 100); // Stagger the animation
+                });
+                observer.unobserve(entry.target); // Stop observing after first trigger
+            }
+        });
+    }, { threshold: 0.3 }); // Trigger when 30% is visible
+
+      const gridContainer = document.querySelector('.services-grid');
+    observer.observe(gridContainer);
+
+
+    const ulineObserver = new IntersectionObserver((entries, observer) => {
+        let entry =  entries[0];
+        if (entry.isIntersecting) {
+            const item = entry.target.querySelector('.uline');
+            setTimeout(() => {
+                item.classList.add('show');
+            }, 150);
+            observer.unobserve(entry.target);
+        }
+    }, {threshold: 0.1});
+
+ulineObserver.observe(document.querySelector('.container'));
+
+
+const careSectionObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            const items = document.querySelectorAll('.content');
+            items.forEach((item, index) => {
+                setInterval(() => {
+                    item.classList.add('show');
+                }, index * 100);
+            }); 
+            observer.unobserve(entry.target);
+        }
+    });
+}, {threshold: 0.3});
+
+careSectionObserver.observe(document.querySelector('.care-section'));
+
+
+const btnObserver = new IntersectionObserver((entries, observer) => {
+    let entry =  entries[0];
+    if (entry.isIntersecting) {
+        const item = entry.target.querySelector('.btn');
+        setTimeout(() => {
+            item.classList.add('show');
+        }, 1000);
+        observer.unobserve(entry.target);
+    }
+}, {threshold: 0.1});
+
+btnObserver.observe(document.querySelector('.text-content'));
 
 
 
